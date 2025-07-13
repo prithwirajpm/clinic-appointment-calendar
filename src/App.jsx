f74a8bb794775a2cd105e00f966d29c553d86146
prithwirajpm <wt.prithwiraj@gmail.com>
2025-07-13 07:07:32 +0530

    Initial commit: Add clinic appointment calendar app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h> Clinic Appoinment </h>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AppointmentModal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AppointmentModal() {
+  return (
+    <div>AppointmentModal</div>
+  )
+}
+
+export default AppointmentModal
